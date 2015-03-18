6e15c32696c1c46fd3203be36a3521e9f2f3bafb
Malte-Christian <scharenberg91@googlemail.com>
2015-03-18 13:25:17 +0100

    "cleanup"

--- FiniteStateModel/ChannelStateMachine.m
+++ FiniteStateModel/ChannelStateMachine.m
@@ -10,15 +10,15 @@ packageNumber = 100;
 throughput(maxNodeNumber) = 0;
 delay(maxNodeNumber) = 0;
 
-for nodeNumber = minNodeNumber:maxNodeNumber
-    fprintf('\nCalculating mean throughput of %d nodes...\n\n', nodeNumber)
+for nNodes = minNodeNumber:maxNodeNumber
+    fprintf('\nCalculating mean throughput of %d nodes...\n\n', nNodes)
     
     % preallocate memory with empty constructor
     clear nodes;
-    nodes(1,nodeNumber) = NodeFiniteStateMachine();
-    for n=1:nodeNumber
-        nodes(n) = NodeFiniteStateMachine();
-        nodes(n).sendPackage(100,4);
+    nodeList(1,nNodes) = NodeFiniteStateMachine();
+    for n=1:nNodes
+        nodeList(n) = NodeFiniteStateMachine();
+        nodeList(n).sendPackage(100,4);
     end
     
     run = true;
@@ -31,7 +31,7 @@ for nodeNumber = minNodeNumber:maxNodeNumber
         throughputSum = 0;
         delaySum = 0;
         ccaFailureSum = 0;
-        for node = nodes
+        for node = nodeList
             if strcmp(node.getState(), 'transmission')
                 channelState = 'busy';
             end
@@ -48,22 +48,22 @@ for nodeNumber = minNodeNumber:maxNodeNumber
         end
         
         if ~run
-            throughput(nodeNumber) = (throughputSum / nodeNumber) /  1000;
-            delay(nodeNumber) = delaySum / nodeNumber;
+            throughput(nNodes) = (throughputSum / nNodes) /  1000;
+            delay(nNodes) = delaySum / nNodes;
             fprintf('CCA Failure sum: %d\n', ccaFailureSum)
-            fprintf('Throughput mean: %f\n', throughput(nodeNumber))
-            fprintf('Delay mean: %f\n', delay(nodeNumber))
+            fprintf('Throughput mean: %f\n', throughput(nNodes))
+            fprintf('Delay mean: %f\n', delay(nNodes))
         end
         
-        for n = 1:nodeNumber
-            nodes(n).nextStep(channelState);
-            if strcmp(nodes(n).getState(), 'idle')
-                packagesSend = nodes(n).getSend() + nodes(n).getNotSend();
+        for n = 1:nNodes
+            nodeList(n).nextStep(channelState);
+            if strcmp(nodeList(n).getState(), 'idle')
+                packagesSend = nodeList(n).getSend() + nodeList(n).getNotSend();
                 if packagesSend <= packageNumber
                     %                         results(n, packagesSend, 1) = slots * 0.000016;
                     %                         results(n, packagesSend, 2) = nodes(n).getThroughput();
                     %nodes(n).reset();
-                    nodes(n).sendPackage(100,4);
+                    nodeList(n).sendPackage(100,4);
                 end
             end
         end
